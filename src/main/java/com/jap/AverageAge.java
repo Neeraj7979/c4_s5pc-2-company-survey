510ffd9a82313d64c39c64f0fa1739a9dcc6ade1
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-24 22:21:56 +0530

    Created method averageAgeCalculator and run the test cases.

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -4,16 +4,21 @@ public class AverageAge {
 
     public String averageAgeCalculator(String[] ageRawData)
     {
+       double sum=0;
         double avg = 0;
 
         try{
             //Write the logic
+            for (int i = 0; i < ageRawData.length; i++) {
+                sum= sum+Double.parseDouble(ageRawData[i]);
+            }
 
             //Handle specific exception
-        }catch (Exception exception)
-        {
+        }catch (NumberFormatException exception) {
+            System.out.println(exception);
             return exception.toString();
         }
+        avg=sum/ageRawData.length;
         return String.valueOf(avg);
     }
 
